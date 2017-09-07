e0cd196cbd4fbca718de148b10f82609cec2b725
Andrew Nicols <andrew@nicols.co.uk>
2017-09-07 14:38:42 +0800

    wip

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 <?php
 
 $dbhost = '127.0.0.1';
-$dbuser = 'travis';
+$dbuser = 'root';
 $dbpass = null;
 $dbname = 'testdb';
 $dbport = 3306;
 $dbsocket = '';
 
 $c = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport, $dbsocket);
-var_dump($c);
+var_dump($c->connect_error);
 
-$dbuser = 'root';
+$dbpass = '';
 $c = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport, $dbsocket);
-var_dump($c);
+var_dump($c->connect_error);
