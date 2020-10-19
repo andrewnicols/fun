3e0c443e138700d651d220ddb9d49e70a6b35325
Andrew Nicols <andrew@nicols.co.uk>
2020-10-19 11:53:22 +0800

    more

--- test.php
+++ test.php
@@ -1,3 +1,27 @@
 <?php
 
+$dirperm = 02777;
+$umaskperm = (($dirperm & 0777) ^ 0777);
+
 var_dump(sys_get_temp_dir());
+
+$newdir = sys_get_temp_dir() . '/newdir/some/other/dir/';
+mkdir($newdir, $dirperm, true);
+
+$fpc = "{$newdir}fpc.zip";
+file_put_contents($fpc, "Some content");
+var_dump(file_get_contents($fpc));
+
+
+$zapath = "{$newdir}archive.zip";
+$za = new ZipArchive();
+$za->open($zapath, ZipArchive::CREATE | ZipArchive::OVERRIDE);
+var_dump($za->getStatusString());
+
+$result = $za->addFile($fpc, basename($fpc));
+var_dump($result);
+var_dump($za->getStatusString());
+
+$za->close();
+var_dump($za->getStatusString());
+var_dump(file_get_contents($zapath));
