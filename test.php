7c941f37988225563ab2e2d2068215b8276e16f7
Andrew Nicols <andrew@nicols.co.uk>
2020-10-19 09:39:33 +0800

    wip

--- test.php
+++ test.php
@@ -1,29 +1,3 @@
 <?php
 
-function fix_html_content($input) {
-    $previouserrors = libxml_use_internal_errors(true);
-
-    $domdoc = new DOMDocument('1.0', 'UTF-8');
-    $domdoc->loadHTML($input, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-
-    libxml_clear_errors();
-    libxml_use_internal_errors($previouserrors);
-
-    var_dump("============================================================================");
-    var_dump($input);
-    var_dump($domdoc->saveHTML());
-
-    $comparewith = <<<EOF
-<p>Hello</p>
-
-EOF;
-
-    if ($domdoc->saveHTML() !== $comparewith) {
-        echo "FAIL /o\\n";
-        exit(1);
-    } else {
-        echo "Pass :)\n";
-    }
-}
-
-fix_html_content('</div><p>Hello</p>');
+var_dump(sys_get_temp_dir());
