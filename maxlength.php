58addef04cb2766daaa8bb9d6a3de45762a9a8e4
Andrew Nicols <andrew@nicols.co.uk>
2020-10-20 12:47:52 +0800

    Maxlength script

---
+++ maxlength.php
@@ -0,0 +1,20 @@
+<?php
+
+$finaldir = str_pad(sys_get_temp_dir() . '/', 261, 'x');
+@mkdir($finaldir, 0777, true);
+$contentfile = "{$finaldir}/example_input.txt";
+unlink($contentfile);
+
+$content = "Some example content";
+file_put_contents($contentfile, $content);
+assert(file_get_contents($contentfile) === $content, "Unable to read back the content");
+
+$za = new ZipArchive();
+$result = $za->open(__DIR__ . '/export.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+assert($result, "Unable to open ZipArchive");
+
+$result = $za->addFile($contentfile);
+assert($result, "Unable to call addFile({$contentfile})");
+
+$result = $za->close();
+assert($result, "Unable to call close()");
